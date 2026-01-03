101f5c90012d599df009e3069fb473fa37d895dc
SpiritSocks <vdteterich@gmail.com>
2026-01-03 16:37:48 +0300

    Started work on the dashboard menu for the homepage

--- frontend/src/shared/components/DashboardMenu/DashboardMenu.tsx
+++ frontend/src/shared/components/DashboardMenu/DashboardMenu.tsx
@@ -1,4 +1,9 @@
 
+import { MousePointerClick, Link2, TrendingUp, Calendar } from "lucide-react";
+
+
+import AnalyticsCard_small from "@/shared/widgets/AnalyticsCard/AnalyticsCard_small";
+
 type DashboardMenuProps = {
     isOpen: boolean;
 }
@@ -8,7 +13,40 @@ const DashboardMenu = ({isOpen}: DashboardMenuProps) => {
     if (!isOpen) return null;
     return (
         <>
-        <h1>This is the dashboard</h1>
+        <section className="flex flex-col items-center gap-10 pt-4 ">
+            <h1 className="font-bold text-md sm:text-3xl">Your Analytics</h1>
+            <div className="flex flex-col sm:flex-row justify-center gap-10">
+
+                <AnalyticsCard_small
+                icon={MousePointerClick}
+                title="Total Clicks"
+                text="0"
+                icon_bgColor="bg-[#4c6fb1]"
+                icon_color="text-white"/>
+
+                <AnalyticsCard_small
+                icon={Link2}
+                title="Total Links"
+                text="0"
+                icon_bgColor="bg-[#c8d69b]"
+                icon_color=""/>
+
+                <AnalyticsCard_small
+                icon={TrendingUp}
+                title="Avg. Per Link"
+                text="0"
+                icon_bgColor="bg-[#f6e6a5]"
+                icon_color=""/>
+
+                <AnalyticsCard_small
+                icon={Calendar}
+                title="Avg. Per Day"
+                text="0"
+                icon_bgColor="bg-[#4c6fb1]"
+                icon_color="text-white"/>
+
+            </div>
+        </section>
         </>
     )
 }
--- frontend/src/shared/widgets/AnalyticsCard/AnalyticsCard_small.tsx
+++ frontend/src/shared/widgets/AnalyticsCard/AnalyticsCard_small.tsx
@@ -1,15 +1,24 @@
-
 import '../AnalyticsCard/AnalyticsCard_small.module.css';
 
-const AnalyticsCard_small = () => {
+type AnalyticsCard_smallProps = {
+    icon: React.ComponentType<{ className?: string }>;
+    title: string;
+    text: string;
+    icon_bgColor: string;
+    icon_color: string;
+}
+
+const AnalyticsCard_small = ({icon: Icon, title, text, icon_bgColor, icon_color}: AnalyticsCard_smallProps) => {
     return (
         <section className="bg-white w-[300px] h-[150px] border-3 
-        border-[#c8d69b] shadow-md flex flex-col justify-center m-10 p-5 rounded-[15px]">
+        border-[#c8d69b] shadow-md flex flex-col justify-center p-5 rounded-[15px]">
             <div className="flex flex-row gap-3 items-center">
-                <img src="dhashd"/>
-                <h3>Total Clicks</h3>
+                <div className={icon_bgColor + " w-11 h-11 flex justify-center items-center rounded-[6px]"}>
+                    <Icon className={icon_color}/>
+                </div>
+                <h3>{title}</h3>
             </div>
-            <p>0</p>
+            <p className='mt-3'>{text}</p>
         </section>
     )
 }
