361a21dfa0124998d5e0180f37653ddc03429859
SpiritSocks <vdteterich@gmail.com>
2026-01-11 23:23:26 +0300

    Changed go.mod

--- backend/internal/config/confing.go
+++ backend/internal/config/confing.go
@@ -13,13 +13,13 @@ type Config struct {
 
 func Load() *Config {
 	return &Config{
-		HTTPPort:   "",
-		DBHost:     "",
-		DBPort:     ,
-		DBUser:     "",
-		DBPassword: "",
-		DBName:     "",
-		ServerPort: ,
-		JWTSecret:  "",
+		HTTPPort:   "8080",
+		DBHost:     "localhost",
+		DBPort:     5432,
+		DBUser:     "user",
+		DBPassword: "password",
+		DBName:     "url_shortener",
+		ServerPort: 8080,
+		JWTSecret:  "your_jwt_secret_key",
 	}
 }
