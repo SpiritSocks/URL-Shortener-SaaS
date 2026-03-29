f436c29acc04f32d1393e17f6d4f8bdf37716a3b
SpiritSocks <vdteterich@gmail.com>
2026-03-29 22:59:56 +0300

    fixes

--- frontend/src/shared/widgets/AnalyticsCard/AnalyticsCard_small.tsx
+++ frontend/src/shared/widgets/AnalyticsCard/AnalyticsCard_small.tsx
@@ -8,19 +8,18 @@ type AnalyticsCard_smallProps = {
     icon_color: string;
 }
 
-const AnalyticsCard_small = ({icon: Icon, title, text, icon_bgColor, icon_color}: AnalyticsCard_smallProps) => {
+const AnalyticsCard_small = ({ icon: Icon, title, text, icon_bgColor, icon_color }: AnalyticsCard_smallProps) => {
     return (
-        <section className="bg-white w-full sm:w-[300px] min-h-[120px] sm:h-[150px] border-3
-        border-[#c8d69b] shadow-md flex flex-col justify-center p-4 sm:p-5 rounded-[15px]">
+        <section className="bg-white w-full min-h-[120px] sm:min-h-[150px] border-3 border-[#c8d69b] shadow-md flex flex-col justify-center p-4 sm:p-5 rounded-[15px]">
             <div className="flex flex-row gap-3 items-center">
-                <div className={icon_bgColor + " w-11 h-11 flex justify-center items-center rounded-[6px]"}>
-                    <Icon className={icon_color}/>
+                <div className={`${icon_bgColor} w-11 h-11 flex justify-center items-center rounded-[6px]`}>
+                    <Icon className={icon_color} />
                 </div>
                 <h3>{title}</h3>
             </div>
-            <p className='mt-3'>{text}</p>
+            <p className="mt-3">{text}</p>
         </section>
-    )
-}
+    );
+};
 
 export default AnalyticsCard_small;
